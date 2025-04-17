df6a49b558b58d32d732f6de35f386cdf7d10cf0
Bithi <ms.bithi@savethechildren.org>
2025-04-17 14:24:49 +0600

    Guote generator begainner friedly project done

--- eventlistener.js
+++ eventlistener.js
@@ -1,3 +1,5 @@
+// Event Listener:
+
 // const buttontwo = document.querySelector("#btn-2");
 
 // function alertbnt(){
@@ -39,6 +41,7 @@
 // revealbtn.addEventListener("click", revealText); 
 
 
+// Event Listener Propagation:
 
 // window.addEventListener("click",function(){
 //     console.log("window clicked");
@@ -70,6 +73,7 @@
 // });
 
 
+// javascript even delegation:
 
 // document.querySelector('#sports').addEventListener("click",function(e){
 // console.log(e.target.getAttribute('id')+'is clicked');
@@ -82,21 +86,89 @@
 // });
 // document.querySelector('#sports li').click();
 
-document.querySelector('#sports').addEventListener("click", function(e) {
-    const target = e.target;
+// document.querySelector('#sports').addEventListener("click", function(e) {
+//     const target = e.target;
 
-    if (target.matches('li')) {
-        // Remove background from all li elements
-        document.querySelectorAll('#sports li').forEach(li => {
-            li.style.backgroundColor = ''; // reset to default
-        });
+//     if (target.matches('li')) {
+//         // Remove background from all li elements
+//         document.querySelectorAll('#sports li').forEach(li => {
+//             li.style.backgroundColor = ''; // reset to default
+//         });
 
-        // Add background to the clicked one
-        target.style.backgroundColor = 'lightgrey';
+//         // Add background to the clicked one
+//         target.style.backgroundColor = 'lightgrey';
 
-        console.log(target.getAttribute('id') + ' is clicked');
-    }
-});
+//         console.log(target.getAttribute('id') + ' is clicked');
+//     }
+// });
 
-// Simulate a click on the first item after page load
-document.querySelector('#sports li').click();
+// // Simulate a click on the first item after page load
+// document.querySelector('#sports li').click();
+
+
+// const sport  = document.querySelector('#sports');
+// const newsport= document.createElement('li');
+
+// newsport.innerHTML="Rugby";
+// newsport.style.border='1px solid white';
+// newsport.style.padding='.5rem';
+// newsport.style.borderRadius='.25rem';
+// newsport.setAttribute('id','rugby');
+
+// sport.appendChild(newsport);
+
+
+// Project 1 : Beginner---
+let btn = document.querySelector('#new-qoute');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = [
+    {
+        quote: `"The best way to find yourself is to lose yourself in the service of others."`,
+        person: `Mahatma Gandhi`
+    },
+    {
+        quote: `"If you want to live a happy life, tie it to a goal, not to people or things."`,
+        person: `Albert Einstein`
+    },
+    {
+        quote: `"Success is not final, failure is not fatal: It is the courage to continue that counts."`,
+        person: `Winston Churchill`
+    },
+    {
+        quote: `"Believe you can and you're halfway there."`,
+        person: `Theodore Roosevelt`
+    },
+    {
+        quote: `"Do not wait for leaders; do it alone, person to person."`,
+        person: `Mother Teresa`
+    },
+    {
+        quote: `"It does not matter how slowly you go as long as you do not stop."`,
+        person: `Confucius`
+    },
+    {
+        quote: `"Your time is limited, so don't waste it living someone else's life."`,
+        person: `Steve Jobs`
+    },
+    {
+        quote: `"Happiness is not something ready made. It comes from your own actions."`,
+        person: `Dalai Lama`
+    },
+    {
+        quote: `"The only limit to our realization of tomorrow is our doubts of today."`,
+        person: `Franklin D. Roosevelt`
+    },
+    {
+        quote: `"In the middle of every difficulty lies opportunity."`,
+        person: `Albert Einstein`
+    },
+];
+
+btn.addEventListener('click', function(){
+    let random = Math.floor(Math.random()* quotes.length);
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+})
